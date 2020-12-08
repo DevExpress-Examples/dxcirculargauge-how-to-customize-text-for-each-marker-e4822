b95475889dc8a92842c2b6ea8763a77eb7f50877
DevExpressExampleBot <fakeEmail>
2020-12-08 19:35:42 +0300

    Source auto update [skip ci]

---
+++ VB/JS/dxSample_Mobile/DxSample/views/index.js
@@ -0,0 +1,62 @@
+﻿DxSample.Index = function (params) {
+
+    var viewModel = {
+        viewShown: function () {
+            $("#chartContainer").dxCircularGauge({
+                scale: {
+                    startValue: 0,
+                    endValue: 60,
+                    majorTick: {
+                        tickInterval: 10
+                    }
+                },
+                rangeContainer: {
+                    backgroundColor: "none",
+                    ranges: [
+                        {
+                            startValue: 0,
+                            endValue: 20,
+                            color: "#A6C567"
+                        },
+                        {
+                            startValue: 20,
+                            endValue: 40,
+                            color: "#FCBB69"
+                        },
+                        {
+                            startValue: 40,
+                            endValue: 60,
+                            color: "#E19094"
+                        }
+                    ]
+                },
+                needles: [{ value: 24 }],
+                markers: [{ value: 5 }, { value: 17 }, { value: 43 }, { value: 55 }],
+                commonMarkerSettings: {
+                    text: {
+                        customizeText: function () {
+                            var result;
+                            switch (this.value) {
+                                case 5:
+                                    result = 'Bad';
+                                    break;
+                                case 17:
+                                    result = 'Not so good';
+                                    break;
+                                case 43:
+                                    result = 'Fairly good';
+                                    break;
+                                case 55:
+                                    result = 'Excellent';
+                                    break;
+                            }
+                            return result;
+                        }
+                    }
+                }
+            });
+        }
+    };
+  
+    return viewModel;
+};
---
+++ VB/Readme.md
@@ -0,0 +1,11 @@
+# dxCircularGauge - How to customize text for each marker 
+<!-- run online -->
+**[[Run Online]](https://codecentral.devexpress.com/e4822/)**
+<!-- run online end -->
+
+
+<p>This example illustrates how to define a different text for different markers via the <a href="http://chartjs.devexpress.com/ApiReference/dxCircularGauge/Configuration/commonMarkerSettings/text#customizeText"><u>dxCircularGauge.commonMarkerSettings.text.customizeText</u></a> callback function.</p>
+
+<br/>
+
+
